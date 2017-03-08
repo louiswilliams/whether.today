299061b2805d93899a164e1664a1be673df6e21c
Louis Williams <louieaw@gmail.com>
2017-03-07 20:00:09 -0500

    Clothing logic and session storage

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,24 +1,69 @@
-var $currentLocation, $whether;
+var $location, $weather, $forecast;
+
+var storageAvailable = false;
+if (typeof(Storage) !== "undefined") {
+  storageAvailable = true;
+}
 
 $(document).ready(function() {
-  $currentLocation = $("#location");
-  $whether = $("#whether");
+  $location = $("#location");
+  $weather = $("#weather");
+  $forecast = $("#forecast");
 
-  getLocation();
+  $location.html("Getting location...");
+
+  var cityId = sessionStorage.getItem("cityId");
+  if (storageAvailable && cityId) {
+    console.log("Session data available");
+    onPosition({
+      cityId: cityId
+    });
+  } else {
+    getLocation();
+  }
 });
 
 function onPosition(position) {
   console.log("Position:", position);
+
+  $location.html("Getting weather information...");
+  // $location.html("(" + position.coords.latitude + ", " + position.coords.longitude + ")");
+
+  var postObj = {};
+  if (position.cityId) {
+    postObj.cityId = position.cityId
+  } else if (position.coords) {
+    postObj.latitude = position.coords.latitude;
+    postObj.longitude = position.coords.longitude;
+  }
+
+  $.post("api/getinfo", postObj, function (data) {
+    if (data.error) {
+      $weather.html("Error getting weather info: ", JSON.stringify(data.error));
+    } else {
+      console.log("result", data);
+      $forecast.html(data.result);
+
+      $weather.html("It's " + data.temp.toFixed(1) + " &deg;F, " + data.description +  ", " + data.windSpeed.toFixed(1) + " MPH winds.");
+      $location.html(data.location);
+
+    if (storageAvailable) {
+      sessionStorage.setItem("cityId", data.cityId);
+    }
+      
+    }
+  });
 }
 
 function onError(error) {
   console.log("Error:", error);
+  $location.html("Error getting location: " + error.message);
 }
 
 function getLocation() {
   if (navigator.geolocation) {
-   	navigator.geolocation.getCurrentPosition(onPosition, onError);
+    navigator.geolocation.getCurrentPosition(onPosition, onError);
   } else {
-    $currentLocation.html("Geolocation not available");
+    $location.html("Geolocation not available");
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,105 @@
 var express = require('express');
+var fs = require('fs');
+var http = require('http');
+var path = require('path');
 var router = express.Router();
+
+var conf = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'conf.json'), 'utf8'));
+
+function clothesFromTemp(fahrenheit) {
+  var result = "";
+  if (fahrenheit > 67) {
+    result += "a T-shirt";
+  } else if (fahrenheit > 52) {
+    result += "a light jacket/sweater";
+  } else if (fahrenheit > 42) {
+    result += "a jacket/light coat";
+  } else if (fahrenheit > 30) {
+    result += "a heavy jacket/coat"; 
+  } else {
+    result += "a heavy coat";
+  }      
+  return result;
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: '' });
 });
+
+/* Get info for location */
+router.post('/api/getinfo', function(req, res) {
+  var latitude, longitude, cityId;
+  if (req.body.cityId) {
+    cityId = parseInt(req.body.cityId);
+  } else {
+    latitude = parseFloat(req.body.latitude);
+    longitude = parseFloat(req.body.longitude);
+  }
+
+  var path = "/data/2.5/weather?APPID=" + conf.weatherApiKey;
+  if (isFinite(cityId)) {
+    path = path + "&id=" + cityId;
+  } else if (isFinite(latitude) && isFinite(longitude)) {
+    path = path + "&lat=" + latitude + "&lon=" + longitude;
+  } else {
+    return res.status(400).send({ message: "(latitude and longitude) or (cityId) fields are required" });
+  }
+
+  http.get({
+    host: "api.openweathermap.org",
+    path: path,
+  }, function (apiRes) {
+    var data = '';
+    apiRes.on('data', function (chunk) {
+      data += chunk;
+    });
+    apiRes.on('end', function() {
+
+      data = JSON.parse(data);
+      console.log(data);
+      var fahrenheit = (9/5 * data.main.temp) - 459.67;
+      var windSpeed  = data.wind.speed * 2.23694;
+
+      var result =  "";
+      var weatherCode = data.weather[0].id;
+      var group = Math.floor(weatherCode/100);
+
+      
+      if (weatherCode == 800) { // Clear skies
+        result = "Clear skies. Wear " + clothesFromTemp(fahrenheit) + ".";
+
+     } else if (weatherCode >= 200 && weatherCode < 600) { // Rain
+        result = "It's raining. Wear "
+               + clothesFromTemp(fahrenheit)
+               + " but bring an umbrella or raincoat."; 
+      } else if (weatherCode >= 600 && weatherCode < 700) { // Snow
+        result = "It's snowing. Wear"
+               + clothesFromTemp(fahrenheit) 
+               + " with boots and a raincoat.";
+      } else if (weatherCode > 800 && weatherCode < 900) { // Clouds
+        result = "It's cloudy. " + clothesFromTemp(fahrenheit);
+      }
+
+     var sendBack = {
+        temp: fahrenheit,
+        windSpeed: windSpeed,
+        location: data.name + ", " + data.sys.country,
+        cityId: data.id,
+        description: data.weather[0].description,
+        result: result
+      }
+      res.send(sendBack);
+    });
+    apiRes.on('error', function(err){ 
+      console.log(err);
+      res.status(500).send({error: err});
+    });
+  });
+
+
+
+});
+
 
 module.exports = router;
